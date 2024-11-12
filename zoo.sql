7c6c8f8607934bee1598f1aa36a4841f52a40b3a
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-12 16:35:19 +0100

    zoo

---
+++ zoo.sql
@@ -0,0 +1,15 @@
+
+--create database zoo;
+
+--drop database zoo;
+
+--use database zoo;
+
+create table zivotinja(
+sifra int,
+vrsta varchar(50),
+ime varchar(50),
+djelatnik int,
+prostorija int,
+datum int
+);
