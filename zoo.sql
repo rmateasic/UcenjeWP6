61af7041733118ca776424e176b14062ff1783c6
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-12 19:49:15 +0100

    Update zoo.sql

--- zoo.sql
+++ zoo.sql
@@ -1,3 +1,7 @@
+-- ovo je SQL skripta zoo
+
+--use master;
+
 
 --create database zoo;
 
@@ -8,7 +12,7 @@
 --create table zivotinja(
 --sifra int,
 --vrsta varchar(50),
---ime varchar(50),
+--ime varchar(30),
 --djelatnik int,
 --prostorija int,
 --datum int
@@ -28,9 +32,9 @@
 --sifra int
 --);
 
-create table djelatnik(
-sifra int,
-ime varchar(50),
-prezime varchar(50),
-iban varchar(50)
-);
+--create table djelatnik(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--iban varchar(100)
+--);
