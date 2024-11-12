7c1a5d8b16bb5320e3df47bd84f26a9dfe4996a5
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-12 19:19:52 +0100

    Kreiranje tablice prostorija

--- zoo.sql
+++ zoo.sql
@@ -5,11 +5,18 @@
 
 --use database zoo;
 
-create table zivotinja(
+--create table zivotinja(
+--sifra int,
+--vrsta varchar(50),
+--ime varchar(50),
+--djelatnik int,
+--prostorija int,
+--datum int
+--);
+
+create table prostorija(
 sifra int,
-vrsta varchar(50),
-ime varchar(50),
-djelatnik int,
-prostorija int,
-datum int
+dimenzije varchar(30),
+maks_broj int,
+mjesto varchar(30)
 );
