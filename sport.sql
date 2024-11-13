55689dbdb567781431e8ab5c160fea873387a6d0
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-13 18:58:18 +0100

    Kreiranje tablice klub

--- sport.sql
+++ sport.sql
@@ -12,3 +12,23 @@ go
 
 use sport;
 go
+
+create table klub(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+osnovan datetime,
+stadion varchar(50) not null,
+predsjednik varchar(50) not null,
+drzava varchar(50) not null,
+liga varchar(50) not null
+);
+
+create table utakmica(
+sifra int not null primary key identity(1,1),
+datum datetime,
+vrijeme datetime,
+lokacija varchar(50) not null,
+stadion varchar(50) not null,
+domaci_klub int not null references klub(sifra),
+gostujuci klub int not null references klub(sifra)
+);
