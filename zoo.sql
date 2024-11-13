83f3878e96811e557e39dabf6adede99e3a09ce3
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-13 17:59:19 +0100

    Rad na bazi zoo

--- zoo.sql
+++ zoo.sql
@@ -12,16 +12,16 @@
 --create table zivotinja(
 --sifra int,
 --vrsta varchar(50),
---ime varchar(30),
+--ime varchar(50),
 --djelatnik int,
-
+--prostorija int
 --datum int
 --);
 
 --create table prostorija(
 --sifra int,
 --dimenzije varchar(30),
-
+--maks_broj int,
 --mjesto varchar(30)
 --);
 
