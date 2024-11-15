c5a2989907293a3469db68e60642066a3838a61b
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-15 13:21:32 +0100

    Kreiranje tablice autor

--- knjznica.sql
+++ knjznica.sql
@@ -12,3 +12,10 @@ go
 
 use knjiznica;
 go
+
+create table autor(
+sifra int not null primary key identity(1,1),
+ime nvarchar(50) not null,
+prezime nvarchar(50) not null,
+datumrodenja datetime
+);
