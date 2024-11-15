7eda7a48259d788303579aaeb3199783fcb5fbdd
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-15 19:00:00 +0100

    Kreiranje baze tipovi auta

---
+++ tipovauta.sql
@@ -0,0 +1,24 @@
+-- Ovo je SQL skripta tipovi auta
+
+use master;
+go
+
+drop database if exists tipoviauta;
+go
+
+create database tipoviauta collate Croatian_CI_AS;
+go
+
+use tipoviauta;
+go
+
+create table proizvodjac(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null,
+zemlja varchar(100) not null
+);
+
+create table vrsteauta(
+sifra int not null primary key identity(1,1),
+naziv varchar(100) not null
+);
