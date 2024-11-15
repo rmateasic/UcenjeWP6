d8dc0b5031380aa644783251f37138115a61b4a1
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-15 14:13:16 +0100

    Kreiranje tablice osobe

--- knjige.sql
+++ knjige.sql
@@ -22,3 +22,11 @@ create table vlasnik(
 sifra int not null primary key identity(11,11),
 knjiga int not null
 );
+
+create table osobe(
+sifra int not null primary key identity(11,11),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+adresa varchar(50) not null,
+email varchar(50) not null,
+);
