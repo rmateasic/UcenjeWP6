5a71c8b67b1ef276dcd4548d2fdb605b2a6206da
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-18 08:26:47 +0100

    Kreiranje tablice automobili

--- tipovauta.sql
+++ tipovauta.sql
@@ -22,3 +22,14 @@ create table vrsteauta(
 sifra int not null primary key identity(1,1),
 naziv varchar(100) not null
 );
+
+create table automobili(
+sifra int not null primary key identity(1,1),
+nazivauta varchar(100) not null ,
+gorivo varchar(100) not null,
+model varchar(100) not null,
+godiste datetime,
+serija varchar(100),
+proizvodjac int not null references proizvodjac(sifra),
+vrsteauta int not null references vrsteauta(sifra)
+);
