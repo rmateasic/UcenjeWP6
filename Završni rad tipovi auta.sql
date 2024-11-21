dd8f4d48ae6e06b27d3c43ce6d157944459dca15
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-21 14:26:28 +0100

    Kreiranje tablice automobili

--- Završni rad tipovi auta.sql
+++ Završni rad tipovi auta.sql
@@ -22,3 +22,14 @@ create table vrsteauta(
 sifra int not null primary key identity(1,1),
 naziv varchar(100) not null
 );
+
+create table automobili(
+sifra int not null primary key identity(1,1),
+nazivauta varchar(100) not null ,
+gorivo varchar(100) not null,
+model varchar(100) not null,
+godiste int,
+serija varchar(100),
+proizvodjaci int not null references proizvodjaci(sifra),
+vrsteauta int not null references vrsteauta(sifra)
+);
