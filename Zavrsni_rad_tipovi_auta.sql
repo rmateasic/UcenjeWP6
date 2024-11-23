26e7d548e1e194dd7aa124604e87ffd04c4b4e59
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-23 10:47:51 +0100

    Update Zavrsni_rad_tipovi_auta.sql

--- Zavrsni_rad_tipovi_auta.sql
+++ Zavrsni_rad_tipovi_auta.sql
@@ -54,37 +54,29 @@ values
 
 insert into vrsteauta(naziv)
 values
-('serija1'), ('serija2'), ('serija3'), ('serija4'), ('serija5'), ('serija6'), ('serija7'), ('serija8'), 
-('Aklasa'), ('Bklasa'), ('Cklasa'), ('Eklasa'), ('Sklasa'), 
-('F40'), ('F50'), ('Dino'), ('Enzo'), 
-('Giulia'), ('Giulietta'), ('Spider'), ('Stelvio'), ('GTV'), 
-('Alteca'), ('Born'), ('Formentor'), ('Leon'), 
-('Ibiza'), ('Cordoba'), ('Malaga'), ('Toledo'), 
-('Megan'), ('Talisman'), ('Capture'), ('Fluence'), 
-('208'), ('308'), ('408'), ('508'), 
-('2008'), ('3008'), ('4008'), ('5008');
+('limuzina'), ('karavan'), ('monovolumen'), ('coupe'), ('kabriolet'), ('suv');
 
 
 insert into automobili(nazivauta, gorivo, model, godiste, proizvodjaci, vrsteauta)
 values
-('Bmw', 'hibrid', 'serija1', null, 1, 1), ('Bmw', 'hibrid', 'serija2', null, 1, 1),
-('Bmw', 'hibrid', 'serija3', null, 1, 1), ('Bmw', 'hibrid', 'serija4', null, 1, 1),
-('Bmw', 'hibrid', 'serija5', null, 1, 1), ('Bmw', 'hibrid', 'serija6', null, 1, 1),
-('Bmw', 'hibrid', 'serija7', null, 1, 1), ('Bmw', 'hibrid', 'serija8', null, 1, 1),
-('Mercedes', 'hibrid', 'Aklasa', null, 2, 2), ('Mercedes', 'hibrid', 'Bklasa', null, 2, 2),
-('Mercedes', 'hibrid', 'Cklasa', null, 2, 2), ('Mercedes', 'hibrid', 'Eklasa', null, 2, 2),
-('Mercedes', 'hibrid', 'Sklasa', null, 2, 2), ('Ferrari', 'hibrid', 'F40', null, 3, 3),
-('Ferrari', 'hibrid', 'F50', null, 3, 3), ('Ferrari', 'hibrid', 'Dino', null, 3, 3),
-('Ferrari', 'hibrid', 'Enzo', null, 3, 3), ('Alfa_romeo', 'hibrid', 'Giulia', null, 4, 4), 
-('Alfa_romeo', 'hibrid', 'Giulietta', null, 4, 4), ('Alfa_romeo', 'hibrid', 'Spider', null, 4, 4),
-('Alfa_romeo', 'hibrid', 'Stelvio', null, 4, 4), ('Alfa_romeo', 'hibrid', 'GTV', null, 4, 4),
-('Cupra', 'hibrid', 'Alteca', null, 5, 5), ('Cupra', 'hibrid', 'Born', null, 5, 5),
-('Cupra', 'hibrid', 'Formentor', null, 5, 5), ('Cupra', 'hibrid', 'Leon', null, 5, 5),
-('Seat', 'hibrid', 'Ibiza', null, 6, 6), ('Seat', 'hibrid', 'Cordoba', null, 6, 6),
-('Seat', 'hibrid', 'Malaga', null, 6, 6), ('Seat', 'hibrid', 'Toledo', null, 6, 6),
-('Renault', 'hibrid', 'Megan', null, 7, 7), ('Renault', 'hibrid', 'Talisman', null, 7, 7),
-('Renault', 'hibrid', 'Capture', null, 7, 7), ('Renault', 'hibrid', 'Fluence', null, 7, 7),
-('Peugeot', 'hibrid', '208', null, 8, 8), ('Peugeot', 'hibrid', '308', null, 8, 8),
-('Peugeot', 'hibrid', '408', null, 8, 8), ('Peugeot', 'hibrid', '508', null, 8, 8),
-('Peugeot', 'hibrid', '2008', null, 8, 8), ('Peugeot', 'hibrid', '3008', null, 8, 8),
-('Peugeot', 'hibrid', '4008', null, 8, 8), ('Peugeot', 'hibrid', '5008', null, 8, 8);
+('Bmw', 'hibrid', 'serija1', '2015' 'Bmw', 'monovolumen'), ('Bmw', 'hibrid', 'serija2', '2015', 'Bmw', 'monovolumen'),
+('Bmw', 'hibrid', 'serija3', '2018', 'Bmw', 'karavan'), ('Bmw', 'hibrid', 'serija4', '2018', 'Bmw', 'limuzina'),
+('Bmw', 'hibrid', 'serija5', '2020', 'Bmw', 'karavan'), ('Bmw', 'hibrid', 'serija6', '2020', 'Bmw', 'coupe'),
+('Bmw', 'hibrid', 'serija7', '2022', 'Bmw', 'suv'), ('Bmw', 'hibrid', 'serija8', '2022', 'Bmw', 'kabriolet'),
+('Mercedes', 'hibrid', 'Aklasa', '2017', 'Mercedes', 'monovolumen'), ('Mercedes', 'hibrid', 'Bklasa', '2017', 'Mercedes', 'monovolumen'),
+('Mercedes', 'hibrid', 'Cklasa', '2019', 'Mercedes', 'limuzina'), ('Mercedes', 'hibrid', 'Eklasa', '2020', 'Mercedes', 'limuzina'),
+('Mercedes', 'hibrid', 'Sklasa', '2022', 'Mercedes', 'kabriolet'), ('Ferrari', 'hibrid', 'F40', '2015', 'Ferrari', 'coupe'),
+('Ferrari', 'hibrid', 'F50', '2017', 'Ferrari', 'kabriolet'), ('Ferrari', 'hibrid', 'Dino', '2018', 'Ferrari', 'coupe'),
+('Ferrari', 'hibrid', 'Enzo', '2020', 'Ferrari', 'suv'), ('Alfa_romeo', 'hibrid', 'Giulia', '2018', 'Alfa_romeo', 'kabriolet'), 
+('Alfa_romeo', 'hibrid', 'Giulietta', '2020', 'Alfa_romeo', 'monovolumen'), ('Alfa_romeo', 'hibrid', 'Spider', '2024', 'Alfa_romeo', 'suv'),
+('Alfa_romeo', 'hibrid', 'Stelvio', '2024', 'Alfa_romeo', 'suv'), ('Alfa_romeo', 'hibrid', 'GTV', '2023', 'Alfa_romeo', 'kabriolet'),
+('Cupra', 'hibrid', 'Alteca', '2020', 'Cupra', 'monovolumen'), ('Cupra', 'hibrid', 'Born', '2022', 'Cupra', 'coupe'),
+('Cupra', 'hibrid', 'Formentor', '2024', 'Cupra', 'monovolumen'), ('Cupra', 'hibrid', 'Leon', '2018', 'Cupra', 'monovolumen'),
+('Seat', 'hibrid', 'Ibiza', '2011', 'Seat', 'monovolumen'), ('Seat', 'hibrid', 'Cordoba', '2016', 'Seat', 'limuzina'),
+('Seat', 'hibrid', 'Malaga', '2014', 'Seat', 'monovolumen'), ('Seat', 'hibrid', 'Toledo', '2011', 'Seat', 'limuzina'),
+('Renault', 'hibrid', 'Megan', '2018', 'Renault', 'limuzina'), ('Renault', 'hibrid', 'Talisman', '2020', 'Renault', 'limuzina'),
+('Renault', 'hibrid', 'Capture', '2022', 'Renault', 'monovolumen'), ('Renault', 'hibrid', 'Fluence', '2022', 'Renault', 'monovolumen'),
+('Peugeot', 'hibrid', '208', '2021', 'Peugeot', 'kabriolet'), ('Peugeot', 'hibrid', '308', '2019', 'Peugeot', 'monovolumen'),
+('Peugeot', 'hibrid', '408', '2020', 'Peugeot', 'limuzina'), ('Peugeot', 'hibrid', '508', '2024', 'Peugeot', 'limuzina'),
+('Peugeot', 'hibrid', '2008', '2019', 'Peugeot', 'suv'), ('Peugeot', 'hibrid', '3008', '2018', 'Peugeot', 'suv'),
+('Peugeot', 'hibrid', '4008', '2019', 'Peugeot', 'suv'), ('Peugeot', 'hibrid', '5008', '2020', 'Peugeot', 'suv');
