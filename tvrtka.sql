b64b32a0c6015ec210290163193226e9d471e09e
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-24 15:23:56 +0100

    Kreiranje baze tvrtka

---
+++ tvrtka.sql
@@ -0,0 +1,22 @@
+﻿-- Ovo je SQL skripta tvrtka
+
+use master;
+go
+
+drop database if exists tvrtka;
+go
+
+create database tvrtka collate Croatian_CI_AS;
+go
+
+use tvrtka;
+go
+
+create table zaposlenici(
+sifra int not null primary key identity(1,1),
+ime varchar(100) not null,
+prezime varchar(100) not null,
+datum_rodenja datetime,
+placa decimal (10,2),
+invalid bit
+);
