7bd6a98fc519c7c64d69a91af1fb2b126cc743a5
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-26 18:57:57 +0100

    kreiranje tablice sestra

--- kolokvij_vjezba_1.sql
+++ kolokvij_vjezba_1.sql
@@ -12,3 +12,12 @@ go
 
 use kolokvij_vjezba_1;
 go
+
+create table sestra(
+sifra int not null primary key identity(1,1),
+introvertno bit not null,
+haljina varchar(31),
+marama decimal(16,6) not null,
+hlace varchar(46),
+narukvica int 
+);
