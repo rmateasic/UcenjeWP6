a71063a851f51aafd7e88a796500a45f756d6ff7
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-26 19:00:42 +0100

    kreiranje tablice punac

--- kolokvij_vjezba_1.sql
+++ kolokvij_vjezba_1.sql
@@ -21,3 +21,10 @@ marama decimal(16,6) not null,
 hlace varchar(46),
 narukvica int 
 );
+
+create table punac(
+sifra int not null primary key identity(1,1),
+ogrlica int not null,
+gustoca decimal(14,9) not null,
+hlace varchar(41) 
+);
