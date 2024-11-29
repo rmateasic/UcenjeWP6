6f3dba0a7c549d6d6ab65b9dcedd23a1391bc788
Robert Mateašić <robert.mateasic@gmail.com>
2024-11-29 15:14:22 +0100

    Kreiranje baze kolokvij_vjezba_2

---
+++ kolokvij_vjezba_2.sql
@@ -0,0 +1,15 @@
+﻿-- ovo je SQL skripta kolokvij_vjezba_2
+
+use master;
+go
+
+drop database if exists kolokvij_vjezba_2;
+go
+
+create database kolokvij_vjezba_2;
+go
+
+
+use kolokvij_vjezba_2;
+go
+
