0fc2cfee643338253aa162d675664e6e9c825332
Robert Mateašić <robert.mateasic@gmail.com>
2024-12-02 11:38:29 +0100

    Kreiranje baze kolokvij_vjezba_3

---
+++ kolokvij_vjezba_3.sql
@@ -0,0 +1,24 @@
+﻿-- ovo je SQL skripta kolokvij_vjezba_3
+
+use master;
+go
+
+drop database if exists kolokvij_vjezba_3;
+go
+
+create database kolokvij_vjezba_3;
+go
+
+
+use kolokvij_vjezba_3;
+go
+
+create table cura(
+sifra int primary key identity(1,1),
+dukserica varchar(49) not null,
+maraka decimal(13,7) not null,
+drugiputa datetime not null,
+majica varchar(49) not null,
+novcica decimal(15,8) not null,
+ogrlica int
+);
