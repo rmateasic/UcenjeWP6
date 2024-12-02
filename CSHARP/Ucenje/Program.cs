1c5a39bce96a41ca068811f6624b4d7413cd23a0
Robert Mateašić <robert.mateasic@gmail.com>
2024-12-02 17:51:20 +0100

    Upoznavanje s sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// komentar jedna linija
+
+/*
+ 
+komentar više linija
+
+*/
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozdrav Svijetu čćšžđ čćšžđ");
+Console.WriteLine("Hello Osijek");
