8f29a5cc24b473dcc04994fbbe517ff9bf6215bd
Robert Mateašić <robert.mateasic@gmail.com>
2024-12-06 14:23:57 +0100

    E03Z1

---
+++ Moja_vjezba_CSHARP_1/Ucenje/E03Z1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+
+    // Program od korisnika unosi dva cijela broja
+    // Program ispisuje u novim redovima
+    // 1. zbroj
+    // .2 razlika drugog i prvog broja
+    // 3. umnožak
+    // 4. zbroj kvadrata prvog i drugog broja
+    // 5. kvocijent prvog i drugog broja
+    internal class E03Z1
+    {
+    }
+}
