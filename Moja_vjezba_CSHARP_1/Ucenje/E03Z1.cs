0ba1a00d0d2f495dd9e0c89b5c3472311343040e
Robert Mateašić <robert.mateasic@gmail.com>
2024-12-06 14:30:47 +0100

    Tidy

--- Moja_vjezba_CSHARP_1/Ucenje/E03Z1.cs
+++ Moja_vjezba_CSHARP_1/Ucenje/E03Z1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-
-namespace Ucenje
+﻿namespace Ucenje
 {
 
     // Program od korisnika unosi dva cijela broja
