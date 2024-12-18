3d46a19ecc4a54bedb09dfc04dcbff528df17f38
Robert Mateašić <robert.mateasic@gmail.com>
2024-12-18 17:50:40 +0100

    E13Rekurzija

---
+++ CSHARP/Ucenje/E13Rekurzija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E13Rekurzija
+    {
+        public static void Izvedi()
+        {
+            // Izvedi(); / ovdje dobijemo stack ovrflow
+
+            Console.WriteLine(Zbroji(100));
+        }
+
+        private static int Zbroji(int broj)
+        {
+            if (broj == 1)
+            {
+                return 1;
+            }
+            return broj + Zbroji(broj-1);
+        }
+        // 100 + 99 + 98 + 97 + ...... + 1
+
+
+    }
+}
