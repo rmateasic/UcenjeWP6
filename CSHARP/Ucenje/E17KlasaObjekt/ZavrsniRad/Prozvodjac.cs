56c1871664f0e10c7d507202eb697f1409888dab
Robert Mateašić <robert.mateasic@gmail.com>
2025-01-13 18:56:59 +0100

    ZavrsniRad

---
+++ CSHARP/Ucenje/E17KlasaObjekt/ZavrsniRad/Prozvodjac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.ZavrsniRad
+{
+    public class Prozvodjac
+    {
+        public int Sifra { get; set; }
+
+    }
+}
---
+++ CSHARP/Ucenje/E17KlasaObjekt/ZavrsniRad/VrsteAuta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje.E17KlasaObjekt.ZavrsniRad
+{
+    public class VrsteAuta
+    {
+        public int Sifra { get; set; }
+
+    }
+}
