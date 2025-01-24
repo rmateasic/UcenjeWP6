e878b8ab8f1f7d4351922016cab75f3d4742ca44
Robert Mateašić <robert.mateasic@gmail.com>
2025-01-24 18:53:36 +0100

    Uvod u Web API

---
+++ CSHARP/WebAPI9/Models/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+
+namespace WebAPI9.Models
+{
+    public class Osoba
+    {
+
+        public int Sifra { get; set; }
+
+        public string? Ime { get; set; }
+
+        public decimal Koeficijent { get; set; }
+
+        public DateTime? DatumRodenja { get; set; }
+
+        public bool Aktivan { get; set; }
+
+
+    }
+}
