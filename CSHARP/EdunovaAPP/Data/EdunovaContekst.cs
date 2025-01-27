46fdd1a2689e280762bb34c6c47e13fbc2b1c963
rmateasic <robert.mateasic@gmail.com>
2025-01-27 18:00:53 +0100

    Priprema za smjer Read

---
+++ CSHARP/EdunovaAPP/Data/EdunovaContekst.cs
@@ -0,0 +1,18 @@
+﻿using EdunovaAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdunovaAPP.Data
+{
+    public class EdunovaContekst : DbContext
+    {
+        public EdunovaContekst(DbContextOptions<EdunovaContekst> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Smjer> Smjerovi { get; set; }
+
+
+
+    }
+}
---
+++ CSHARP/EdunovaAPP/Models/Smjer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EdunovaAPP.Models
+{
+    public class Smjer: Entitet
+    {
+        public string Naziv { get; set; } = "";
+        [Column("cijena")]  // ovo je bit EF, mogućnost mapiranja
+
+        public decimal? CijenaSmjera { get; set; }
+
+        public DateTime? IzvodiSeOd { get; set; }
+
+        public bool Vaucer { get; set; }
+
+
+    }
+}
